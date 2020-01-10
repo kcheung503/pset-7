bfa881006c3d90ea53fbca3d52da1c91d5312561
wood503 <kwoodc503@gmail.com>
2020-01-09 20:51:43 -0500

    student.java working

--- src/com/apcsa/model/Student.java
+++ src/com/apcsa/model/Student.java
@@ -4,25 +4,34 @@ import com.apcsa.model.User;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+public class Student extends User {
+	private int studentId;
+    private int classRank;
+    private int gradeLevel;
+    private int graduationYear;
+    private double gpa;
+    private String firstName;
+    private String lastName;
+    
 /**
  * Creates an instance of the Student class.
  * 
  * @param user
  * @param rs
  * @throws SQLException
- */
-
-public Student(User user, ResultSet rs) throws SQLException {
-    super(user);
+ */ 
     
-    this.studentId = rs.getInt("student_id");
-    this.classRank = rs.getInt("class_rank");
-    this.gradeLevel = rs.getInt("grade_level");
-    this.graduationYear = rs.getInt("graduation");
-    this.gpa = rs.getDouble("gpa");
-    this.firstName = rs.getString("first_name");
-    this.lastName = rs.getString("last_name");
-}}
+    public Student(User user, ResultSet rs) throws SQLException {
+    	super(user);
+    	
+    	this.studentId = rs.getInt("student_id");
+    	this.classRank = rs.getInt("class_rank");
+    	this.gradeLevel = rs.getInt("grade_level");
+    	this.graduationYear = rs.getInt("graduation");
+    	this.gpa = rs.getDouble("gpa");
+    	this.firstName = rs.getString("first_name");
+    	this.lastName = rs.getString("last_name");
+    }
 
 	public int getStudentId() {
 		return studentId;
