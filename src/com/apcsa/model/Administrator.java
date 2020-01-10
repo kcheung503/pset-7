77728b83aea9398d6d9d96a89d110b1385816c9b
wood503 <kwoodc503@gmail.com>
2020-01-09 20:16:57 -0500

    public administrator

--- src/com/apcsa/model/Administrator.java
+++ src/com/apcsa/model/Administrator.java
@@ -10,3 +10,20 @@ public class Administrator extends User {
     private String jobTitle;
 
 }
+
+/**
+ * Creates an instance of the Administrator class.
+ * 
+ * @param user
+ * @param rs
+ * @throws SQLException
+ */
+    
+public Administrator(User user, ResultSet rs) throws SQLException {
+    super(user);
+    
+    this.administratorId = rs.getInt("administrator_id");
+    this.firstName = rs.getString("first_name");
+    this.lastName = rs.getString("last_name");
+    this.jobTitle = rs.getString("job_title");
+}
