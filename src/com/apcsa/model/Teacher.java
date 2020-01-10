92c4a55ed25cf9af1460be56c97c5c30dc31a5f9
wood503 <kwoodc503@gmail.com>
2020-01-09 20:44:01 -0500

    teacher.java should be working

--- src/com/apcsa/model/Teacher.java
+++ src/com/apcsa/model/Teacher.java
@@ -1,26 +1,28 @@
 package com.apcsa.model;
 
-import com.apcsa.model.User;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/**
- * Creates an instance of the Teacher class.
- * 
- * @param user
- * @param rs
- * @throws SQLException
- */
+import com.apcsa.model.User;
 
-public Teacher(User user, ResultSet rs) throws SQLException {{
-    super(user);
+public class Teacher extends User {
+
+	private int teacherId;
+    private int departmentId;
+    private String firstName;
+    private String lastName;
+    private String departmentName;
+
     
-    this.teacherId = rs.getInt("teacher_id");
-    this.departmentId = rs.getInt("department_id");
-    this.firstName = rs.getString("first_name");
-    this.lastName = rs.getString("last_name");
-    this.departmentName = rs.getString("department_id");
-}}
+    public Teacher(User user, ResultSet rs) throws SQLException {
+        super(user);
+        
+        this.teacherId = rs.getInt("teacher_id");
+        this.departmentId = rs.getInt("department_id");
+        this.firstName = rs.getString("first_name");
+        this.lastName = rs.getString("last_name");
+        this.departmentId = rs.getInt("department_id");
+    }
 
 
     public int getTeacherId() {
